b87a88b25baff5b33bec481bb1b78b4cf51b29b6
Géraud CONTINSOUZAS <gcs@cpan.org>
2015-09-16 10:19:44 +1100

    initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::Setup::Flavor::GCS - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::Setup::Flavor::GCS;
+
+# DESCRIPTION
+
+Amon2::Setup::Flavor::GCS is ...
+
+# LICENSE
+
+Copyright (C) Geraud CONTINSOUZAS.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Geraud CONTINSOUZAS &lt;gcs@cpan.org&gt;
---
+++ lib/Amon2/Setup/Flavor/GCS.pm
@@ -0,0 +1,39 @@
+package Amon2::Setup::Flavor::GCS;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::Setup::Flavor::GCS - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::Setup::Flavor::GCS;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::GCS is ...
+
+=head1 LICENSE
+
+Copyright (C) Geraud CONTINSOUZAS.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Geraud CONTINSOUZAS E<lt>gcs@cpan.orgE<gt>
+
+=cut
+
